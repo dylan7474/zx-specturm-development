5d550a557f5346532cbb65143b01801a777a497f
dylan7474 <112658365+dylan7474@users.noreply.github.com>
2026-01-09 23:01:23 +0000

    Update test.c

--- test.c
+++ test.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <arch/zx.h>
 #include <string.h>
+#include <input.h>
 
 void main(void) {
-    printf("%c", 12);
-
-    zx_border(1);
-
-    printf("\x16\x05\x05Hello Speccy dev!");
-    printf("\n\n    Building from Garuda...");
-
-    memset((void*)22528, 14, 768);
-
+    // 1. Setup the screen
+    printf("%c", 12);               // Clear screen
+    zx_border(1);                   // Set border to blue
+    
+    printf("\x16\x05\x05Advanced Speccy Dev");
+    printf("\n\n    Press 'B' for Blue Border");
+    printf("\n    Press 'R' for Red Border");
+    printf("\n    Press 'G' for Green Border");
+    
+    // 2. Main Input Loop
     while (1) {
+        if (in_key_pressed(IN_KEY_SCANCODE_b)) {
+            zx_border(1); // Blue
+        }
+        if (in_key_pressed(IN_KEY_SCANCODE_r)) {
+            zx_border(2); // Red
+        }
+        if (in_key_pressed(IN_KEY_SCANCODE_g)) {
+            zx_border(4); // Green
+        }
     }
 }
